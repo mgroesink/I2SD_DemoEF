03a17b2b3a69ae50e0aed06fa3ff40405ce4fae4
Marcel Roesink <mroesink@rocvantwente.nl>
2024-11-15 08:41:46 +0100

    New project added for overloading

---
+++ Overloading/Controllers/CarController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Overloading.Models;
+
+namespace Overloading.Controllers
+{
+    public class CarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(string? make, string? model, int? year)
+        {
+            Car car;
+            if(year == null && string.IsNullOrEmpty(make) && string.IsNullOrEmpty(model))
+            {
+                car = new();
+            }
+            else if (year == null && string.IsNullOrEmpty(model))
+            {
+                car = new Car(make);
+            }
+            else if (year == null)
+            {
+                car = new Car(make,model);
+            }
+            else
+            {
+                car = new Car(make, model, year);
+            }
+            return View(car);
+        }
+    }
+}
---
+++ Overloading/Models/Car.cs
@@ -0,0 +1,37 @@
+﻿namespace Overloading.Models
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int? Year { get; set; }
+
+        public Car(string make, string model, int? year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public Car(string make, string model)
+        {
+            Make = make;
+            Model = model;
+            Year = 0;
+        }
+
+        public Car(string make)
+        {
+            Make = make;
+            Model = "Unknown";
+            Year = 0;
+        }
+
+        public Car()
+        {
+            Make = "Unknown";
+            Model = "Unknown";
+            Year = 0;
+        }
+    }
+}
